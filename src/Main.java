d29285ae41a498fff51bccddf9908a2db6b7c26b
shtblgel@mail.ru <shtblgel@mail.ru>
2024-05-11 18:37:50 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        var allWeight = boxer1Weight + boxer2Weight;
+        System.out.println("Общий вес " + allWeight + " кг");
+        var differenceWeight = boxer2Weight - boxer1Weight;
+        System.out.println("Разница в весе " + differenceWeight + " кг");
+        differenceWeight = boxer2Weight % boxer1Weight;
+        System.out.println("Остаток от деления между весами " + differenceWeight);
+        var allWorkTime = 640;
+        var oneWorkTime = 8;
+        var worker = allWorkTime / oneWorkTime;
+        System.out.println("Всего работников в компании - " + worker + " человек");
+        var newWorker = worker + 94;
+        var newAllWorkTime = newWorker * oneWorkTime;
+        System.out.println("Если в компании работает " + newWorker + " человека, то всего " + newAllWorkTime + " часа работы может быть поделено между сотрудниками");
     }
 }
